b88d973def61b927a6128c1c1f09046a840858e3
rich abs <rishi_sandhu@outlook.com>
2024-09-24 14:58:49 +0000

    DARK MODE WIP

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ for (let i = 0; i < title.length; i++) {
     setTimeout(() => charAdd(i), 1000*i)
     console.log(title.charAt(i)) // debug
 }
+
+function themeToggle() {
+    document.querySelector("body").classList.toggle("dark")
+    console.log("Theme switched")
+}
